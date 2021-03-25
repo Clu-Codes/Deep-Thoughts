681ef44079659bea2aca01429705a415b1d46e81
Clu <retsulcconnor@gmail.com>
2021-03-25 00:04:13 -0400

    added routes for multi-page feel

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 // gets data when you're ready to use it
 import ApolloClient from 'apollo-boost';
+// BrowserRouter and Route are components provided by React Router library
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Home from './pages/Home';
 
@@ -18,13 +25,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
+      {/* wrapped div element in Router component to make child components on page aware of client-side routing that can now take place */}
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/profile/:username?' component={Profile} />
+              <Route exact path='/thought/:id' component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
